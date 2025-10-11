8f8573ea81d5e4d5032001a2d046b0aa55a529b0
kdaff830 <darenka.2007@icloud.com>
2025-10-11 12:41:29 +0300

    Kuznetsova Daria dz

---
+++ площадь квадрата.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+int main()
+{
+	int a;
+	std::cout << "The side of the square equals "; //storona kvadrata ravna
+	std::cin >> a;
+	std::cout << "\n" << "The area of the square equals " << a * a << "." << std::endl; //plochad kvadrata ravna
+	return 0;
+}
