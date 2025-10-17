dee15440bfb63e55af001afc7dde5d524cfb2663
kdaff830 <darenka.2007@icloud.com>
2025-10-17 21:01:41 +0300

    mini hw1 done

--- sem1/КузнецоваДА/homework_1/площадь квадрата.cpp
+++ sem1/КузнецоваДА/homework_1/площадь квадрата.cpp
@@ -1,12 +1,12 @@
 ﻿#include <iostream>
 
 int main(){
-	int side_length = 0;
-	std::cout << "The side of the square equals ";
-	std::cin >> side_length;
+  int side_length = 0;
+  std::cout << "The side of the square equals ";
+  std::cin >> side_length;
 
-	const int area = side_length * side_length;
-	std::cout << "\n" << "The area of the square equals " << area << "." << std::endl;
+  const int area = side_length * side_length;
+  std::cout << "\n" << "The area of the square equals " << area << "." << std::endl;
 
-	return 0;
+  return 0;
 }
--- sem1/КузнецоваДА/homework_1/площадь трапеции.cpp
+++ sem1/КузнецоваДА/homework_1/площадь трапеции.cpp
@@ -2,17 +2,17 @@
 
 int main()
 {
-	float base_one = 0.0f;
-	float base_two = 0.0f;
-	float height = 0.0f;
+  float base_one = 0.0f;
+  float base_two = 0.0f;
+  float height = 0.0f;
 
-	std::cout << "The bases of the trapezoid equal: ";
-	std::cin >> base_one >> base_two;
-	std::cout << "The height of the trapezoid equals: ";
-	std::cin >> height;
+  std::cout << "The bases of the trapezoid equal: ";
+  std::cin >> base_one >> base_two;
+  std::cout << "The height of the trapezoid equals: ";
+  std::cin >> height;
 
-	const float area = (base_one + base_two) / 2.0f * height;
-	std::cout << "The area of the trapezoid equals " << area << std::endl;
+  const float area = (base_one + base_two) / 2.0f * height;
+  std::cout << "The area of the trapezoid equals " << area << std::endl;
 
-	return 0;
+  return 0;
 }
