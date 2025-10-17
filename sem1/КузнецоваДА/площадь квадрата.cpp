7a16626668b9c28a7f73157e27c7ed318390b4f1
kdaff830 <darenka.2007@icloud.com>
2025-10-17 20:18:05 +0300

    mini hw1 done

---
+++ sem1/КузнецоваДА/площадь квадрата.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main(){
+	int side_length = 0;
+	std::cout << "The side of the square equals ";
+	std::cin >> side_length;
+
+	const int area = side_length * side_length;
+	std::cout << "\n" << "The area of the square equals " << area << "." << std::endl;
+
+	return 0;
+}
