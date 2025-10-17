d0dc06d2ecd288f244cfba0c830adecd218cd9f5
kdaff830 <darenka.2007@icloud.com>
2025-10-17 20:05:52 +0300

    mini hw1 done

---
+++ sem1/КузнецоваДА/homework_1/площадь трапеции.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main(){
+	float base_one = 0.0f;
+	float base_two = 0.0f;
+	float height = 0.0f;
+
+	std::cout << "The bases of the trapezoid equal: ";
+	std::cin >> base_one >> base_two;
+	std::cout << "The height of the trapezoid equals: ";
+	std::cin >> height;
+
+	const float area = (base_one + base_two) / 2.0f * height;
+	std::cout << "The area of the trapezoid equals " << area << std::endl;
+
+	return 0;
+}
